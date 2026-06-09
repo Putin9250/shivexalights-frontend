cbaa4afaa50b2c045b1b02852671f5d6a0ef63a6
Putin9250 <sahil24042005@gmail.com>
2026-06-09 18:31:26 +0530

    Updates

--- assets/makeRequest-a7388ab8.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index-9bd421b5.js";const s=e.create({baseURL:"https://shivexalights-backend.onrender.com/api"});export{s as m};
--- assets/useFetch-85c0df3e.js
+++
@@ -1,1 +0,0 @@
-import{r as t}from"./index-9bd421b5.js";import{m as u}from"./makeRequest-a7388ab8.js";const h=e=>{const[r,o]=t.useState([]),[c,a]=t.useState(!1),[n,f]=t.useState(!1);return t.useEffect(()=>{(async()=>{try{a(!0);const s=await u.get(e);o(s.data||[])}catch{f(!0)}a(!1)})()},[e]),{data:r,loading:c,error:n}};export{h as u};
