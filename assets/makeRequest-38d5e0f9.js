4b822d9ec7770e373a810ece3ab9d900c49f4194
Putin9250 <sahil24042005@gmail.com>
2026-06-16 11:53:52 +0530

    Updates

---
+++ assets/makeRequest-38d5e0f9.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-f24bda5e.js";const s=e.create({baseURL:"https://shivexalights-backend.onrender.com/api"});export{s as m};
--- assets/makeRequest-7056c271.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index-d0909e81.js";const s=e.create({baseURL:"https://shivexalights-backend.onrender.com/api"});export{s as m};
